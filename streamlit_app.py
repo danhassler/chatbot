dbd1055dafda430c4d170213c34401b0887336de
Dan <dpjhassler@gmail.com>
2025-10-01 12:02:34 -0700

    history

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,8 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel("gemini-2.5-flash")
-    chat = model.start_chat(history = st.session_state.messages)
+    chat = client.chat.create(model="gemini-2.5-flash")
     response = chat.send_message(
-        model="gemini-2.5-flash",
-        history=st.session_state.messages,
         contents=prompt + " is my request, respond to me as if I were an nurse."
         + " and that I am focused on nurse wellness." + "Respond as if a southern lousiana accent." 
     )
