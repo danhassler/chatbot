c5a7a23f8c5e98a1b7b9a2f27fae3b000a9a6a9d
Dan <dpjhassler@gmail.com>
2025-10-01 12:17:18 -0700

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,10 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
-    chat = client.aio.chats.create(model="gemini-2.5-flash")
-    response = chat.send_message(prompt + " is my request, respond to me as if I were an nurse."
-        + " and that I am focused on nurse wellness." 
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt + " is my request, respond to me as if I were an nurse."
+        + " and that I am focused on nurse wellness." + "Respond as if a southern lousiana accent." 
     )
     st.session_state.messages.append({"role": "assistant", "content": response.text})
     st.markdown(response.text)
